abeb0457d44171d3c6021fb1751b0f1c2fd320c2
Baptiste Leduc <baptiste@weglot.com>
2018-06-01 13:23:00 +0200

    removing useless global options

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app.Usage = "Simple multiple project manager"
 	app.Version = "1.0.0-alpha"
 	app.Copyright = "(c) 2018 Baptiste Leduc"
-	app.UsageText = "slurp [-Vhv] <project> \n\t slurp init [-Vhv] <project>\n\t slurp burp [-Vhv]"
+	app.UsageText = "slurp <project> \n\t slurp init <project>\n\t slurp burp"
 
 	app.Commands = []cli.Command{
 		{
