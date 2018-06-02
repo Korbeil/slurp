a3ed31899dc1a8fa7205ca7accdea04b0a8b24cf
Baptiste Leduc <baptiste@weglot.com>
2018-06-02 21:41:26 +0200

    adding json utils

---
+++ utils/json/json.go
@@ -0,0 +1,23 @@
+package json
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// WriteJsonInFile write given struct as Json in given file (create it if needed)
+func WriteJsonInFile(s interface{}, path string) {
+	b, err := json.Marshal(s)
+	if err != nil {
+		fmt.Printf("Can't parse JSON for file `%s`", path)
+		os.Exit(1)
+	}
+
+	err = ioutil.WriteFile(path, b, os.ModePerm)
+	if err != nil {
+		fmt.Printf("Can't write JSON file `%s`", path)
+		os.Exit(1)
+	}
+}
