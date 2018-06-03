1aa9710541862c2de64e81b0d8be8671dcaf98ce
Baptiste Leduc <baptiste@weglot.com>
2018-06-03 22:37:58 +0200

    cleaning since utility func are out of this file

--- commands/in.go
+++ commands/in.go
@@ -2,9 +2,7 @@ package commands
 
 import (
 	"github.com/urfave/cli"
-	utilsJson "github.com/Korbeil/slurp/utils/json"
 	"github.com/Korbeil/slurp/utils/directory"
-	"encoding/json"
 	"os"
 	"fmt"
 )
@@ -28,37 +26,10 @@ func InCommandAction(c *cli.Context) error {
 
 	project := loadProject(homeDir, projectName)
 
+	// Setting new bash_history
+	os.Setenv("HISTFILE", homeDir+"/.slurp/projects/"+projectName+"/bash_history")
+
 	print(project.Directory)
 
 	return nil
-}
-
-func loadProject(homeDir string, projectName string) Config {
-	var b []byte
-	b = utilsJson.ReadJson(homeDir+"/.slurp/projects/"+projectName+"/config.json")
-
-	var conf Config
-	json.Unmarshal(b, &conf)
-	return conf
-}
-
-func loadEnv(homeDir string) Environment {
-	envPath := homeDir+"/.slurp/env.json"
-
-	// env doesn't exists, let's create it and return an empty one
-	if directory.CheckExists(envPath) == false {
-		env := Environment{Project: "", OldBashHistoryPath: os.Getenv("HISTFILE")}
-		utilsJson.WriteJsonInFile(
-			env,
-			envPath)
-		return env
-	}
-
-	// or just read it :)
-	var b []byte
-	b = utilsJson.ReadJson(envPath)
-
-	var env Environment
-	json.Unmarshal(b, &env)
-	return env
 }
