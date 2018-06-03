5555e52da6d3a5563ff7907f536e84fb4d6b6825
Baptiste Leduc <baptiste@weglot.com>
2018-06-03 20:13:00 +0200

    removing struct & adding some of command behavior (wip)

--- commands/in.go
+++ commands/in.go
@@ -1,10 +1,45 @@
 package commands
 
-import "github.com/urfave/cli"
+import (
+	"github.com/urfave/cli"
+	utilsJson "github.com/Korbeil/slurp/utils/json"
+	"github.com/Korbeil/slurp/utils/directory"
+	"encoding/json"
+)
 
 // InCommandAction is used as default action
 // Also, it permits to jump from one project to another
 func InCommandAction(c *cli.Context) error {
-	cli.ShowAppHelp(c)
+	homeDir := directory.UserHome()
+	projectName := c.Args().First()
+	env := loadEnv(homeDir)
+
+	// If no project name given, we take last project we loaded from env
+	if projectName == "" {
+		projectName = env.Project
+	}
+
+	project := loadProject(homeDir, projectName)
+
+	print(project.Directory)
+
 	return nil
+}
+
+func loadProject(homeDir string, projectName string) Config {
+	var b []byte
+	b = utilsJson.ReadJson(homeDir+"/.slurp/projects/"+projectName+"/config.json")
+
+	var conf Config
+	json.Unmarshal(b, &conf)
+	return conf
+}
+
+func loadEnv(homeDir string) Environment {
+	var b []byte
+	b = utilsJson.ReadJson(homeDir+"/.slurp/env.json")
+
+	var env Environment
+	json.Unmarshal(b, &env)
+	return env
 }
